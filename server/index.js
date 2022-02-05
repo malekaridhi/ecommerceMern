0d12481328f26d692d7c4dad854ccec36ba07b52
malekaridhi <malek.aridhii@gmail.com>
2022-02-05 07:59:49 +0100

    models

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+
+
+
+require("dotenv").config();
+const app = express()
+const http = require("http").createServer(app);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+;
+require('dotenv').config();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+
+
+mongoose.connect(
+    process.env.MONGODB_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    //   useCreateIndex: true,
+    },
+    (err) => {
+      if (err) throw err;
+      console.log("MONGO DB is connected on port : 8000");
+    }
+  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const PORT = process.env.PORT || 8000;
+http.listen(PORT, () => console.log(`server is running on port : ${PORT}`));
---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    img: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("user", User);
