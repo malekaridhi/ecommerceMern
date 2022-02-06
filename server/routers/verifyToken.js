4a250d5190baab84d6e8aa16862bd8671dfa6773
malekaridhi <malek.aridhii@gmail.com>
2022-02-06 10:10:25 +0100

    jwt

--- server/routers/verifyToken.js
+++ server/routers/verifyToken.js
@@ -6,6 +6,8 @@ const jwt =require('jsonwebtoken');
 const verifyToken = (req,res,next)=>{
     const authHeader = req.headers.token;
     if (authHeader){
+        // if were using bearer we need to split  the token
+        const token = authHeader.split(" ")[1]
      jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
          if(err){res.status(403).json("not a valid token ")
          //assign user to the req
