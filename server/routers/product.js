79612dba84c6c19ae9af6f214a4df2ad39e606df
malekaridhi <malek.aridhii@gmail.com>
2022-02-06 18:37:20 +0100

    product

--- server/routers/product.js
+++ server/routers/product.js
@@ -47,27 +47,27 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
     }
   })
 // //GET
-// router.get("/find/:id",verifyTokenAndAdmin,async (req,res)=>{
-//     try {
-//         const user = await User.findById(req.params.id)
-//         const { password, ...others } = user._doc;
-//          res.status(200).json(others);
-//     } catch (err) {
-//        res.status(500).json(err)
-//     }
-//   })
-// // GET ALL USERS
+router.get("/find/:id",async (req,res)=>{
+    try {
+        const product = await Product.findById(req.params.id)
+        
+         res.status(200).json(product);
+    } catch (err) {
+       res.status(500).json(err)
+    }
+  })
+// // GET ALL PRODUCT
 
-// router.get("/",verifyTokenAndAdmin,async (req,res)=>{
-//     const query = req.query.new;
-//     try {
-//         const users = query? await User.find().sort({_id:-1}).limit(5)
-//         :await User.find()
-//          res.status(200).json(users);
-//     } catch (err) {
-//        res.status(500).json(err)
-//     }
-//   })
+router.get("/",async (req,res)=>{
+    const query = req.query.new;
+    try {
+        const products = query? await Product.find().sort({_id:-1}).limit(5)
+        :await Product.find()
+         res.status(200).json(products);
+    } catch (err) {
+       res.status(500).json(err)
+    }
+  })
 
 // //GET USER STATS
 // router.get('/stats', async(req,res)=>{
