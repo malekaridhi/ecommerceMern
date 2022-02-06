97e6d6e07466450e068a92170c301987e72f2192
malekaridhi <malek.aridhii@gmail.com>
2022-02-06 10:07:43 +0100

    jwt

--- server/routers/user.js
+++ server/routers/user.js
@@ -8,8 +8,22 @@ const {
 
 
 
-router.put('/:id',verifyToken,(req,res)=>{
-
+router.put('/:id',verifyTokenAndAuthorization,async(req,res)=>{
+if (req.body.password){
+    req.body.password= CryptoJS.DES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
+}
+try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+        $set : req.body
+        //take everything iside req.body and set it again
+    },{new:true})
+    res.status(200).json(updatedUser)
+} catch (err) {
+    res.status(500).json(err)
+}
 })
 
 
