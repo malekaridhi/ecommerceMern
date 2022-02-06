9624bc0ae59ba6db6083fc3bf24ed498feaf7c2b
malekaridhi <malek.aridhii@gmail.com>
2022-02-06 11:40:36 +0100

    get

--- server/routers/user.js
+++ server/routers/user.js
@@ -59,7 +59,16 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
        res.status(500).json(err)
     }
   })
-
+//GET
+router.get("/find/:id",verifyTokenAndAdmin,async (req,res)=>{
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc;
+         res.status(200).json(others);
+    } catch (err) {
+       res.status(500).json(err)
+    }
+  })
 
 
 
