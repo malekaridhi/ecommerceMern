60da4668b7c37342eab75fdd3d87e8d8cc8b58b1
malekaridhi <malek.aridhii@gmail.com>
2022-02-06 09:07:49 +0100

    jwt

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,53 +1,58 @@
-const express = require('express')
+const express = require("express");
 const router = require("express").Router();
-const User = require('../models/User')
-const CryptoJS = require("crypto-js")
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 //REGISTER
 
-router.post("/register",async(req,res)=>{
-    const newUser = new User ({
-        username: req.body.username,
-        email: req.body.email,
-        password:CryptoJS.DES.encrypt(req.body.password, process.env.PASS_SEC).toString()
-    });
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.DES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
 
-    try {
-        const savedUser = await newUser.save()
-        res.status(201).json(savedUser)
-    } catch (err) {
-      res.status(500).json(err)  
-    }
-})
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.post("/login",async(req,res)=>{
-try {
-    const user = await User.findOne({username : req.body.username})
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
 
-    !user && res.status(401).json('no user found')
+    !user && res.status(401).json("no user found");
 
-    const hasdPassword = CryptoJS.DES.decrypt(user.password, process.env.PASS_SEC);
-    const originalPassword = hasdPassword.toString(CryptoJS.enc.Utf8)
+    const hasdPassword = CryptoJS.DES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const originalPassword = hasdPassword.toString(CryptoJS.enc.Utf8);
 
-    originalPassword !== req.body.password && res.status(401).json('no user found')
-
-    const { password, ...others } = user._doc
-      res.status(200).json(others)
-} catch (err) {
-    console.log(err)
-}
-}) 
-
-
+    originalPassword !== req.body.password &&
+      res.status(401).json("no user found");
 
 
+    const accessToken = jwt.sign({
+        id : user._id,
+        isAdmin :user.isAdmin
+    },
+     process.env.JWT_SEC,
+     {expiresIn:"3d"}
+    )  
 
 
-
-
-
-
-
-
-
+    const { password, ...others } = user._doc;
+    res.status(200).json({...others,accessToken});
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
