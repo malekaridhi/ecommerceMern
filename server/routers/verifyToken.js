54222b47f02360ccc483786717de694370c8e58e
malekaridhi <malek.aridhii@gmail.com>
2022-02-06 11:20:15 +0100

    fixing errors

--- server/routers/verifyToken.js
+++ server/routers/verifyToken.js
@@ -1,23 +1,21 @@
 const jwt =require('jsonwebtoken');
 
 
-
-
 const verifyToken = (req,res,next)=>{
-    const authHeader = req.headers.token;
-    if (authHeader){
-        // if were using bearer we need to split  the token
-        const token = authHeader.split(" ")[1]
-     jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
-         if(err){res.status(403).json("not a valid token ")
-         //assign user to the req
-          req.user=user;
-          next();
-        }
-     })
-    }else{
-        return res.status(401).json("your not authentication")
-    }
+  const authHeader = req.headers.token;
+  if (authHeader){
+      // if were using bearer we need to split  the token
+      const token = authHeader.split(" ")[1]
+   jwt.verify(token,process.env.JWT_SEC,(err,user)=>{
+       if(err)res.status(403).json("not a valid token ")
+       //assign user to the req
+        req.user=user;
+        next();
+      
+   })
+  }else{
+      return res.status(401).json("your not authentication")
+  }
 }
 const verifyTokenAndAuthorization = (req, res, next) => {
     verifyToken(req, res, () => {
