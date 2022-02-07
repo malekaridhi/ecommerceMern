1fbd299c107f7729008d417cab06cb38b4979dc4
malekaridhi <malek.aridhii@gmail.com>
2022-02-07 14:23:17 +0100

    get all order

--- ecommerce/src/components/pay.jsx
+++ ecommerce/src/components/pay.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
 const Pay = () => {
+    const onToken = (token) =>{
+       console.log(token) 
+    }
     return ( <>
+    <StripeCheckout name="Banana Shop" 
+    image="https://img2.freepng.fr/20180519/rya/kisspng-logo-phoenix-art-5afffc96998f80.692522331526725782629.jpg"
+    billingAddress
+    shippingAddress
+    description='your total is $20'
+    amount={2000}
+    token={onToken}
+    stripeKey={process.env.KEY}
+    >
      <button>pay</button>
+
+    </StripeCheckout>
     </> );
 }
  
