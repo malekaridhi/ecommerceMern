56c9e4f736611689e4c1ca7af478d75c95106cb7
malekaridhi <malek.aridhii@gmail.com>
2022-02-07 07:02:50 +0100

    update order

--- server/routers/order.js
+++ server/routers/order.js
@@ -18,20 +18,20 @@ const {
     }
   });
   //PUT
-//   router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//     try {
-//       const updatedCart = await Cart.findByIdAndUpdate(
-//         req.params.id,
-//         {
-//           $set: req.body,
-//         },
-//         { new: true }
-//       );
-//       res.status(200).json(updatedCart);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+  router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+      const updatedOrder = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedOrder);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   
 //   //DELETE
 //   router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
