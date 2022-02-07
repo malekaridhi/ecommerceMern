eb1c57eb4795027aa698e140d5e5a33062394840
malekaridhi <malek.aridhii@gmail.com>
2022-02-07 06:47:13 +0100

    cart

--- server/routers/cart.js
+++ server/routers/cart.js
@@ -1,10 +1,89 @@
 const express = require('express')
 const router = require("express").Router();
+const Cart = require("../models/Cart")
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
+} =require('./verifyToken')
 
-// router.get("/usertest",(req,res)=>{
-//     res.send("usertest is a sucess")
-// })
-
+//CREATE
+router.post("/",verifyToken,async(req,res)=>{
+    const newCart = new Cart (req.body)
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart)
+    } catch (err) {
+        res.status(500).json(err);  
+    }
+    })
+//     //PUT
+//     router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+     
+//         try {
+//           const updatedProduct= await Product.findByIdAndUpdate(
+//             req.params.id,
+//             {
+//               $set: req.body,
+//             },
+//             { new: true }
+//           );
+//           res.status(200).json(updatedProduct);
+//         } catch (err) {
+//           res.status(500).json(err);
+//         }
+//       });
+    
+    
+//       //DELETE
+//       router.delete("/:id",verifyTokenAndAdmin,async (req,res)=>{
+//         try {
+//             await Product.findByIdAndDelete(req.params.id)
+//             res.status(200).json("product deleted")
+//         } catch (err) {
+//            res.status(500).json(err)
+//         }
+//       })
+//     // //GET
+//     router.get("/find/:id",async (req,res)=>{
+//         try {
+//             const product = await Product.findById(req.params.id)
+            
+//              res.status(200).json(product);
+//         } catch (err) {
+//            res.status(500).json(err)
+//         }
+//       })
+//     // // GET ALL PRODUCT
+    
+//     router.get("/",async (req,res)=>{
+//         const qNew = req.query.new;
+//         const qCategory = req.query.category
+//         try {
+//             let products;
+//             if(qNew){
+//                 products = await Product.find().sort({createdAt: -1}).limit(5)
+//             } else if (qCategory){
+//                 products = Product.find({
+//                     categories: {
+//                         $in:[qCategory]
+//                     }
+//                 })
+//             }else {
+//                 products = await Product.find()
+//             }
+//              res.status(200).json(products);
+//         } catch (err) {
+//            res.status(500).json(err)
+//         }
+//       })
+    
+    
+    
+    
+    
+    
+    
 
 
 
--- server/routers/product.js
+++ server/routers/product.js
@@ -80,28 +80,7 @@ router.get("/",async (req,res)=>{
     }
   })
 
-// //GET USER STATS
-// router.get('/stats', async(req,res)=>{
-// const date = new Date()
-// const lastYear = new Date(date.setFullYear(date.getFullYear()-1))
-// try {
-//     const data = await User.aggregate([
-//         {$match: { createdAt: {$gte:lastYear } } },
-//         {$project:{
-//             month: {$month:"$createdAt"},
-//         }},
-//         {
-//             $group: {
-//                 _id: "$month",
-//                 tolat: {$sum:1},
-//             }
-//         }
-//     ]);
-//     res.status(200).json(data)
-// } catch (err) {
-//     res.status(500).json(err)
-// }
-// })
+
 
 
 
