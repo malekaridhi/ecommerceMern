778193bfe4f81e86473ad6e11b6079256e67550f
malekaridhi <malek.aridhii@gmail.com>
2022-02-11 16:55:07 +0100

    navbar

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import Stack from '@mui/material/Stack';
+import MailIcon from '@mui/icons-material/Mail';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 const Container = styled.div`
 height :60px;
 `;
@@ -26,13 +30,27 @@ align-items: center;
 margin-left: 25px;
 padding: 5px
 `
+const Input =styled.input`
+  border:none;
+`
 const Center = styled.div`
 flex:1;
+text-align: center;
 `;
+const Logo = styled.h1`
+  font-weight:bold;
+`
 const Right = styled.div`
 flex:1;
+display: flex;
+align-items: center;
+justify-content: flex-end
 `;
-
+const MenuItem = styled.div`
+font-size: 14px;
+cursor: pointer;
+margin-left: 25px
+`
 const Navbar = () => {
   return (
     <>
@@ -41,12 +59,20 @@ const Navbar = () => {
           <Left>
               <Language>EN</Language>
               <SearchContainer>
-                  input
-                  <SearchIcon/>
+                 <Input/>
+                  <SearchIcon style={{color:"gray",fontSize:16}}/>
               </SearchContainer>
           </Left>
-          <Center>Center</Center>
-          <Right>Right</Right>
+          <Center><Logo>BANANA</Logo></Center>
+          <Right>
+              <MenuItem>REGISTER</MenuItem>
+              <MenuItem>SIGN IN</MenuItem>
+              <MenuItem>
+              <Badge badgeContent={4} color="secondary">
+        <ShoppingCartOutlinedIcon color="action" />
+      </Badge>
+              </MenuItem>
+         </Right>
         </Wrapper>
       </Container>
     </>
