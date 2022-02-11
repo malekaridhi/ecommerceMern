baa787a6a93a24732ffdc357caa181273cf2c769
malekaridhi <malek.aridhii@gmail.com>
2022-02-11 14:07:12 +0100

    styling

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router,Route, Routes} from "react-router-dom"
 import Pay from "./components/pay"
 import Success from "./components/success";
 import Home from "./pages/Home"
-import Navbar from "./components/Navbar"
 function App() {
   return (
    <>
@@ -14,7 +13,7 @@ function App() {
     <Route exact path="/success" element={<Success/>} />
    </Routes>
    </Router> */}
-   <Navbar></Navbar>
+  <Home/>
    
    </>
   );
--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import styed from "styled-components"
+import React from "react";
+import styed from "styled-components";
 const Container = styed.div`
 height :60px;
-background-color:black;
-
+`;
+const Wrapper = styed.div`
+padding: 10px 20px;
+display: flex;
+justify-content: space-between;
+`;
+const Left = styed.div`
+flex:1;
+`;
+const Language =styed.span`
+font-size: 10px;
+currsor: pointer;
 `
+const Center = styed.div`
+flex:1;
+`;
+const Right = styed.div`
+flex:1;
+`;
 
 const Navbar = () => {
-    return ( <>
-    <Container>
-        navbar
-    </Container>
-    </> );
-}
- 
+  return (
+    <>
+      <Container>
+        <Wrapper>
+          <Left>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo maiores deleniti architecto placeat fugit ut corporis officia pariatur laborum eos aut velit id quas, ab voluptatibus voluptatem sapiente soluta blanditiis?</Left>
+          <Center>Center</Center>
+          <Right>Right</Right>
+        </Wrapper>
+      </Container>
+    </>
+  );
+};
+
 export default Navbar;
