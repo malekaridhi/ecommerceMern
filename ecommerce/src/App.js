32f06e46a5ebe94ec9dad534f4af5f7028196afa
malekaridhi <malek.aridhii@gmail.com>
2022-02-11 13:40:10 +0100

    client navbar

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,6 +2,8 @@
 import { BrowserRouter as Router,Route, Routes} from "react-router-dom"
 import Pay from "./components/pay"
 import Success from "./components/success";
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
 function App() {
   return (
    <>
@@ -12,7 +14,7 @@ function App() {
     <Route exact path="/success" element={<Success/>} />
    </Routes>
    </Router> */}
-   <div>Hello world</div>
+   <Navbar></Navbar>
    
    </>
   );
---
+++ ecommerce/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styed from "styled-components"
+const Container = styed.div`
+height :60px;
+background-color:black;
+
+`
+
+const Navbar = () => {
+    return ( <>
+    <Container>
+        navbar
+    </Container>
+    </> );
+}
+ 
+export default Navbar;
