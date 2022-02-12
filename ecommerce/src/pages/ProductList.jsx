3238b5ac992ae5ccb6cf7d8f91b0a2faa4404279
malekaridhi <malek.aridhii@gmail.com>
2022-02-12 19:23:41 +0100

    ProductList

--- ecommerce/src/pages/ProductList.jsx
+++ ecommerce/src/pages/ProductList.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
+import Products from '../components/Products'
+import NewsLetter from '../components/NewsLetter'
 import Footer from '../components/Footer'
 const Container = styled.div`
 
 `;
 const FilterContainer = styled.div`
+display:flex;
+justify-content:space-between;
 
 `
 const Title = styled.h1`
-
+margin:20px;
 `
 const Filter = styled.div`
+margin:20px;
 
 `
 
@@ -27,6 +32,9 @@ const PoductList = () => {
             <Filter>filter1</Filter>
             <Filter>filter2</Filter>
         </FilterContainer>
+        <Products/>
+        <NewsLetter/>
+        <Footer/>
         </Container>
         </>
      );
