7b056abea07e02c0078c92122ff30e5a2cd2175a
malekaridhi <malek.aridhii@gmail.com>
2022-02-12 12:49:54 +0100

    slyder data

--- ecommerce/src/data.js
+++ ecommerce/src/data.js
@@ -3,6 +3,9 @@ import image1 from "./images/a1.jpg"
 import image2 from "./images/a2.jpg"
 import image3 from "./images/a3.jpg"
 import image4 from "./images/a4.jpg"
+import image5 from "./images/b1.jpg"
+import image6 from "./images/b2.jpg"
+import image7 from "./images/b3.jpg"
 export const sliderItems = [
     {
       id: 1,
@@ -27,4 +30,23 @@ export const sliderItems = [
     },
   ];
 
-  
+  export const categories = [
+    {
+      id: 1,
+      img: image5,
+      title: "SPORT STYLE!",
+      cat:"sport"
+    },
+    {
+      id: 2,
+      img: image6,
+      title: "DARK JACKETS",
+      cat:"coat"
+    },
+    {
+      id: 3,
+      img: image7,
+      title: "PARTY DRESSES",
+      cat:"women"
+    },
+  ];
