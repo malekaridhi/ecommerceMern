d37e49abe6058814618e3d151bcc42bf76a5265b
malekaridhi <malek.aridhii@gmail.com>
2022-02-12 19:35:28 +0100

    ProductList page

--- ecommerce/src/pages/ProductList.jsx
+++ ecommerce/src/pages/ProductList.jsx
@@ -20,6 +20,18 @@ const Filter = styled.div`
 margin:20px;
 
 `
+const FilterText = styled.span`
+font-size:20px;
+font-weight:600;
+margin-right:20px;
+`
+const Select = styled.select`
+padding: 10px;
+margin-right: 20px;
+`;
+const Option = styled.option`
+
+`
 
 const PoductList = () => {
     return ( 
@@ -29,8 +41,32 @@ const PoductList = () => {
         <Navbar/>
         <Title>Dresses</Title>
         <FilterContainer>
-            <Filter>filter1</Filter>
-            <Filter>filter2</Filter>
+            <Filter><FilterText>Filter Products:</FilterText>
+            <Select name="color">
+            <Option disabled>Color</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
+          </Select>
+          <Select name="size" >
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+            </Filter>
+            <Filter><FilterText>Sort Products:</FilterText>
+            <Select >
+            <Option value="newest">Newest</Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="desc">Price (desc)</Option>
+          </Select>
+            </Filter>
         </FilterContainer>
         <Products/>
         <NewsLetter/>
