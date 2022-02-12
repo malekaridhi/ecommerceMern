02d994183fc9e430709244b3e14c5bb4c05ddc47
malekaridhi <malek.aridhii@gmail.com>
2022-02-12 13:01:21 +0100

    slyder data

--- ecommerce/src/components/CategoryItem.jsx
+++ ecommerce/src/components/CategoryItem.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+flex:1;
 `
+const Image =styled.img`
+
+`
+const Info = styled.div`
+
+`
+const Title =styled.h1`
+
+`
+
+const Button = styled.button`
+
+`
+
 const CategoryItem = ({item}) => {
     return ( 
         <>
         <Container>
-            hello
+            <Image src={item.img}/>
+            <Info>
+               <Title>{item.title}</Title>
+               <Button>SHOP NOW</Button>
+            </Info>
         </Container>
         </>
      );
