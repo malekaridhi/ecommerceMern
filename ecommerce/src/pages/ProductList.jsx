a704530bbc96119e7e30f46424c70b7638900370
malekaridhi <malek.aridhii@gmail.com>
2022-02-12 19:04:24 +0100

    ProductList

---
+++ ecommerce/src/pages/ProductList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+`;
+const PoductList = () => {
+    return ( 
+        <>
+        <Container>
+            
+        </Container>
+        </>
+     );
+}
+ 
+export default PoductList;
