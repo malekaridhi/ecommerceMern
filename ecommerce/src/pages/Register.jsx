bccfc30ebd67fc13aa88a0c537b0fe28c3126061
malekaridhi <malek.aridhii@gmail.com>
2022-02-13 19:26:15 +0100

    register

--- ecommerce/src/pages/Register.jsx
+++ ecommerce/src/pages/Register.jsx
@@ -1,17 +1,60 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/896291/pexels-photo-896291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+width: 40%;
+padding:20px;
+background-color:white;
+`;
+const Title = styled.h1`
+font-size: 24px;
+font-weight: 300;
 
-`
+`;
+const Form = styled.form`
+display: flex;
+flex-wrap:wrap;
+`;
+const Input = styled.input``;
+const Agreement = styled.span``;
+const Button = styled.button``;
 
 const Register = () => {
-    return ( 
-        <>
-        <Container>
-            
-        </Container>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Container>
+        <Wrapper>
+          <Title>CREATE AN ACCOUNT</Title>
+          <Form>
+            <Input placeholder="name" />
+            <Input placeholder="last name" />
+            <Input placeholder="username" />
+            <Input placeholder="email" />
+            <Input placeholder="password" />
+            <Input placeholder="confirm password" />
+            <Agreement>
+              By creating an account, I consent to the processing of my personal
+              data in accordance with the <b>PRIVACY POLICY</b>
+            </Agreement>
+            <Button>CREATE</Button>
+          </Form>
+        </Wrapper>
+      </Container>
+    </>
+  );
+};
+
 export default Register;
