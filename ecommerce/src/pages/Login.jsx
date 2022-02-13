737bc26033fe0fffb55b62506536ec26fd6af6f4
malekaridhi <malek.aridhii@gmail.com>
2022-02-13 18:51:57 +0100

    register

--- ecommerce/src/pages/Login.jsx
+++ ecommerce/src/pages/Login.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-const Container = styled.div`
-
-`
+const Container = styled.div``;
+const Wrapper = styled.div``;
+const Title = styled.h1``;
+const Form = styled.form``;
+const Input = styled.input``;
+const Agreement = styled.span``;
+const Button = styled.button``;
 
 const Login = () => {
-    return ( 
-        <>
-        <Container>
-            
-        </Container>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+
 export default Login;
