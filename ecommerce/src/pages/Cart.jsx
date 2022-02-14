09cdc5c50844f22b0d7c861eef740d3e2e7ee726
malekaridhi <malek.aridhii@gmail.com>
2022-02-14 21:15:16 +0100

    cart

--- ecommerce/src/pages/Cart.jsx
+++ ecommerce/src/pages/Cart.jsx
@@ -113,6 +113,10 @@ font-weight:200;
 
 const SummaryItem =styled.div`
 margin: 30px 0px;
+display:flex;
+justify-content: space-between;
+font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
 `
 const SummaryItemText = styled.span``;
 const SummaryItemPrice = styled.span``;
