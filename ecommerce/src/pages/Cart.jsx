842e5c9c58e5717721bfe8b00c6beedf02c6c932
malekaridhi <malek.aridhii@gmail.com>
2022-02-14 19:31:33 +0100

    cart

--- ecommerce/src/pages/Cart.jsx
+++ ecommerce/src/pages/Cart.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Footer from '../components/Footer';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 const Container = styled.div`
 
 `
@@ -11,11 +13,11 @@ const Title = styled.h1``;
 const Top = styled.div``;
 const TopButton = styled.button``;
 const TopTexts = styled.div``;
-const topText = styled.span``;
+const TopText = styled.span``;
 const Bottom = styled.div``;
 const Info = styled.div``;
 const Product = styled.div``;
-const PrudctDetail = styled.div``;
+const ProdcutDetail = styled.div``;
 const Image = styled.img``;
 const Detail = styled.div``;
 const ProductName = styled.span``;
@@ -27,6 +29,8 @@ const ProductAmountContainer = styled.div``;
 const ProductAmount = styled.div``;
 const ProductPrice = styled.div``;
 const Hr = styled.hr``;
+const Summary = styled.div``;
+const SummaryTitle = styled.h1``
 const summaryItem = styled.div``;
 const summaryItemText = styled.span``;
 const summaryItemPrice = styled.span``;
@@ -39,7 +43,63 @@ const Cart = () => {
         <Container>
         <Announcement/>
         <Navbar/>
-        cart 
+        <Wrapper>
+            <Title>YOUR BAG</Title>
+            <Top>
+                <TopButton></TopButton>
+                    <TopTexts>
+                       <TopText></TopText>
+                       <TopText></TopText>
+                    </TopTexts>
+                <TopButton></TopButton>
+                
+            </Top>
+            <Bottom>
+                <Info>
+                    <Product>
+                        <ProdcutDetail>
+                            <Image/>
+                            <Detail>
+                                <ProductName></ProductName>
+                                <ProductId></ProductId>
+                                <ProductColor></ProductColor>
+                                <ProductSize></ProductSize>
+                            </Detail>
+                        </ProdcutDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                                <AddIcon/>
+                                <ProductAmount></ProductAmount>
+                                <RemoveIcon/>  
+                            </ProductAmountContainer>
+                            <ProductPrice>
+
+                            </ProductPrice>
+                        </PriceDetail>
+                    </Product>
+                    <Hr/>
+                </Info>
+                <Summary>
+                    <SummaryTitle></SummaryTitle>
+                    <summaryItem>
+                        <summaryItemText></summaryItemText>
+                        <summaryItemPrice></summaryItemPrice>
+                    </summaryItem>
+                    <summaryItem>
+                        <summaryItemText></summaryItemText>
+                        <summaryItemPrice></summaryItemPrice>
+                    </summaryItem>
+                    <summaryItem>
+                        <summaryItemText></summaryItemText>
+                        <summaryItemPrice></summaryItemPrice>
+                    </summaryItem>
+                    <summaryItem>
+                        <summaryItemText></summaryItemText>
+                        <summaryItemPrice></summaryItemPrice>
+                    </summaryItem>
+                </Summary>
+            </Bottom>
+        </Wrapper>
        <Footer/>
         </Container>
         </>
