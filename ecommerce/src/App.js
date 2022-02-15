c6ce6b15a3d462010f01c441057164c12bfdaa9e
malekaridhi <malek.aridhii@gmail.com>
2022-02-15 13:31:18 +0100

    responsive

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -19,7 +19,7 @@ function App() {
    </Routes>
    </Router> */}
   {/* <Home/> */}
-  <Home/>
+  < Home/>
    
    </>
   );
--- ecommerce/src/responsive.js
+++ ecommerce/src/responsive.js
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
   return css`
-    @media only screen and (min-width: 321px) {
+    @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
       ${props}
     }
   `;
