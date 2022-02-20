8cb9bcab560bc7eb5f0fb031d2050b0ffe5bdd15
malekaridhi <malek.aridhii@gmail.com>
2022-02-20 23:17:39 +0100

    user

--- ecommerce/src/redux/reducers/index.js
+++ ecommerce/src/redux/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'
 import  cartReducer  from '../cartRedux';
+import userReducer from '../userRedux'
 const rootReducer = combineReducers({
   cart:cartReducer,
- 
+  user:userReducer,
    });
    
    export default rootReducer
