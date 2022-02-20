24fbec4cf4132462a3e6d8769648213336bdb05d
malekaridhi <malek.aridhii@gmail.com>
2022-02-20 22:56:12 +0100

    stripe chekout

--- ecommerce/src/components/success.jsx
+++ ecommerce/src/components/success.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 const Success = () => {
+    const location = useLocation()
     return (<>
     <h1> congrats your payment is sucessful </h1>
     </>  );
