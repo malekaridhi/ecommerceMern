84c437cd917af02bc1ab92b27ecd8625ff865230
malekaridhi <malek.aridhii@gmail.com>
2022-02-20 23:02:46 +0100

    stripe chekout

--- ecommerce/src/components/success.jsx
+++ ecommerce/src/components/success.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import styled from "styled-components";
+
 import { useLocation } from 'react-router-dom';
-const Container = styled.div``
+
 const Success = () => {
     const location = useLocation()
     console.log(location)
