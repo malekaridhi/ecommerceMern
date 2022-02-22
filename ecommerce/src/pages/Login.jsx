af1ecd4862844fb33ddbb2f1e3a7c3ce59f95a18
malekaridhi <malek.aridhii@gmail.com>
2022-02-22 20:22:48 +0100

    login

--- ecommerce/src/pages/Login.jsx
+++ ecommerce/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { mobile } from "../responsive";
 import { login } from "../redux/reducers/apiReq";
@@ -17,66 +17,68 @@ const Container = styled.div`
   justify-content: center;
 `;
 const Wrapper = styled.div`
-width: 25%;
-padding:20px;
-background-color:white;
-${mobile({ width: "75%" })}
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+  ${mobile({ width: "75%" })}
 `;
 
 const Title = styled.h1`
-font-size: 24px;
-font-weight: 300;
-border-left: 6px solid #f58840; 
+  font-size: 24px;
+  font-weight: 300;
+  border-left: 6px solid #f58840;
 `;
 const Form = styled.form`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 const Input = styled.input`
-flex: 1;
-min-width: 40%;
-margin: 10px  0px;
-padding: 10px;
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
 `;
 
 const Button = styled.button`
-width: 100%;
-border: none;
-padding: 15px 20px;
-background-color: #f58840;
-color: white;
-cursor: pointer;
+  width: 100%;
+  border: none;
+  padding: 15px 20px;
+  background-color: #f58840;
+  color: white;
+  cursor: pointer;
 `;
 const Link = styled.a`
-margin: 5px 0px;
+  margin: 5px 0px;
   font-size: 12px;
   text-decoration: underline;
   cursor: pointer;
 `;
 const Error = styled.span``;
 const Login = () => {
- const [username,setUsername]= useState("")
- const [password,setPassword]= useState("")
-  const handleLogin = (e) =>{
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
     e.preventDefault();
-  }
+  };
   return (
     <>
       <Container>
-          <Wrapper>
-              <Title>LOGIN</Title>
-              <Form>
-                  <Input placeholder="username"
-                  onChange={(e)=> setUsername(e.target.value)}
-                  />
-                  <Input placeholder="password"
-                  onChange={(e)=>setPassword(e.target.username)}
-                  />
-                <Button onClick={handleLogin()}>LOGIN</Button>
-                <Link>FORGET PASSWORD ??</Link>
-                <Link>CREATE NEW ACCOUNT </Link>
-              </Form>
-          </Wrapper>
+        <Wrapper>
+          <Title>LOGIN</Title>
+          <Form>
+            <Input
+              placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.username)}
+            />
+            <Button onClick={handleLogin}>LOGIN</Button>
+            <Link>FORGET PASSWORD ??</Link>
+            <Link>CREATE NEW ACCOUNT </Link>
+          </Form>
+        </Wrapper>
       </Container>
     </>
   );
