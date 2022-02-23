c26ac27ef82e7d8178b10915958fc3577aecfc0c
malekaridhi <malek.aridhii@gmail.com>
2022-02-23 22:37:28 +0100

    admin sidebar

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,15 +1,17 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css"
+import Home from "./pages/home/Home";
 function App() {
   return (
     <div>
     <Topbar/>
     <div className="container">
       <Sidebar/>
-      <div className="others">
+      {/* <div className="others">
       otherpages
-      </div>
+      </div> */}
+      <Home/>
       
     </div>
     </div>
---
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./featuredInfo.css"
+
+const FeaturedInfo = () => {
+    return ( 
+        <div className='featuredInfo'>
+              <div className="featuredItem">
+                  
+              </div>
+        </div>
+     );
+}
+ 
+export default FeaturedInfo;
