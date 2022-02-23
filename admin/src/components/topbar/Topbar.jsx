9f70a0b135206b3a553e58b2e7d1d4b897adedd7
malekaridhi <malek.aridhii@gmail.com>
2022-02-23 15:43:08 +0100

    admin navbar

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./topbar.css"
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
 import image from "../../images/logo4.png"
 const Topbar = () => {
     return ( <>
@@ -9,8 +12,9 @@ const Topbar = () => {
               <img className='logo' src={image}/>
           </div>
           <div className="topRight">
-              <div className="topbarIcon">
-                  
+              <div className="topbarIconContainer">
+                  <NotificationsNoneIcon/>
+                  <span className='topIconBadge'>2</span>
               </div>
           </div>
       </div>
