0e62ed6352a151c3029e1dddb3858ed09a926cb7
malekaridhi <malek.aridhii@gmail.com>
2022-02-23 22:45:06 +0100

    home

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./featuredInfo.css"
-
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 const FeaturedInfo = () => {
     return ( 
         <div className='featuredInfo'>
@@ -9,7 +10,11 @@ const FeaturedInfo = () => {
                       Revenue
                   </span>
                   <div className="featuredMoneyContainer">
-                      
+                      <span className="featuredMoney">250 DT</span>
+                      <span className="featuredMoneyRate">-11.4
+                       <ArrowDownwardIcon/>
+                      </span>
+                       <span className="featuredSub">Cpmpared to last mounth</span>
                   </div>
               </div>
         </div>
