a89ef3ef6c4cbc6b21659c6123c92e9abed2e653
malekaridhi <malek.aridhii@gmail.com>
2022-02-23 11:33:29 +0100

    login

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -1,27 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
+
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import thunk from 'redux-thunk';
-import reportWebVitals from './reportWebVitals';
-import rootReducer from './redux/reducers';
+import thunk from "redux-thunk";
+import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./redux/reducers";
+import { PersistGate } from "redux-persist/integration/react";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-   
-  )
-);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+let persistor = persistStore(store);
 ReactDOM.render(
-  <Provider  store={store} >
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
