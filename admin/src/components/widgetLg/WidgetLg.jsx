ffe84f43bc7c7a4545c4c7040616c8627c21d8db
malekaridhi <malek.aridhii@gmail.com>
2022-02-24 21:53:51 +0100

    widget

--- admin/src/components/widgetLg/WidgetLg.jsx
+++ admin/src/components/widgetLg/WidgetLg.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import "./widgetLg.css"
+import image from "../../images/b2.jpg"
 const WidgetLg = () => {
     return ( 
         <div className="widgetLg">
-             large
+             <h3 className="widgetLgTitle">Latest Transactions</h3>
+             <table className="widgetLgTable">
+                  <tr className="widgetLTr">
+                       <th className="widgetLgTh">Customer</th>
+                       <th className="widgetLgTh">Date</th>
+                       <th className="widgetLgTh">Amount</th>
+                       <th className="widgetLgTh">Status</th>    
+                  </tr>
+                    <tr className="widgetLgTr">
+                         <td className="widgetLgUser">
+                              <img src={image} alt="" className="widgetLgImg" />
+                         </td>
+                    </tr>
+             </table>
         </div>
      );
 }
