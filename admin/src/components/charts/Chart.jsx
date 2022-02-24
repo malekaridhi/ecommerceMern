befba020f81546a2359e89287adafca130164545
malekaridhi <malek.aridhii@gmail.com>
2022-02-24 15:34:47 +0100

    info

---
+++ admin/src/components/charts/Chart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./chart.css"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const Chart = () => {
+    return ( 
+        <div className='chart'>
+          <h3 className="chartTitle">
+              Salse Analytics
+          </h3>
+        </div>
+     );
+}
+ 
+export default Chart;
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Chart from "../../components/charts/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 const Home = () => {
   return <div className="home">
       <FeaturedInfo/>
+      <Chart/>
   </div>;
 };
 
