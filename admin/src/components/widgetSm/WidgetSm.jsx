a88c9e2974aa1d2eb7b3be8024a077fefd8571e6
malekaridhi <malek.aridhii@gmail.com>
2022-02-24 21:25:09 +0100

    widget

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./widgetSm.css"
+import image from "../../images/a4.jpg"
 const WidgetSm = () => {
     return ( 
         <div className="widgetsm">
-         small
+         <span className="widgetsmTitle">New Join Members</span>
+         <ul className="widgetsmList">
+             <li className="widgetsmItem">
+                 <img src={image} alt="" className="widgetsmImg" />
+                  <div className="widgetsmUser">
+                      <span className="widgetsmUsername">Ross Geller</span>
+                      <span className="widgetsmUserTitle">Teacher</span>
+
+                  </div>
+                  <button className="widgetsmButton"></button>
+             </li>
+         </ul>
         </div>
      );
 }
