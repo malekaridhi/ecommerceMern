c61604df744d931b8dff80a7761b3f396e85fb3f
malekaridhi <malek.aridhii@gmail.com>
2022-02-25 23:00:28 +0100

    userlist

---
+++ admin/src/pages/userList/UserList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./userList.css"
+const UserList = () => {
+    return ( 
+        <div className="userList">
+
+        </div>
+     );
+}
+ 
+export default UserList;
