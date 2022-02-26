b0599d04a42d56b4d23d33f9ff2da8c4ec5cf4f9
malekaridhi <malek.aridhii@gmail.com>
2022-02-26 22:11:43 +0100

    userlist route

--- admin/src/pages/userList/UserList.jsx
+++ admin/src/pages/userList/UserList.jsx
@@ -1,49 +1,109 @@
-import React from 'react';
-import "./userList.css"
-import { DataGrid } from '@mui/x-data-grid';
-import image from "../../images/a2.jpg"
+import React from "react";
+import "./userList.css";
+import { DataGrid } from "@mui/x-data-grid";
+import image from "../../images/a2.jpg";
 const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    { field: 'username', headerName: 'Username', width: 200 },
-    { field: 'email', headerName: 'Email', width: 200 },
-    {
-      field: 'status',
-      headerName: 'Status',
-      width: 120,
-    },
-    {
-        field: 'transaction',
-        headerName: 'Transaction Volume',
-        width: 190,
-      },
-    ,
-  ];
-  
-  const rows = [
-    { id: 1, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
-    { id: 2, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
-    { id: 3, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
-    { id: 4, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
-    { id: 5, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
-    { id: 6, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
-    { id: 7, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
-
-  ];
-  
-const UserList = () => {
-    return ( 
-        <div className="userList">
-<div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="userListUser">
+          <img className="userListImg" src={params.row.avatar} alt="" />
+          {params.row.username}
         </div>
-     );
-}
- 
+      );
+    },
+  },
+  { field: "email", headerName: "Email", width: 200 },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 120,
+  },
+  {
+    field: "transaction",
+    headerName: "Transaction Volume",
+    width: 190,
+  },
+  ,
+];
+
+const rows = [
+  {
+    id: 1,
+    username: "Jon Snow",
+    avatar: `${ image }`,
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "500DT",
+  },
+  {
+    id: 2,
+    username: "Jon Snow",
+    avatar: `${ image }`,
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "500DT",
+  },
+  {
+    id: 3,
+    username: "Jon Snow",
+    avatar: `${ image }`,
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "500DT",
+  },
+  {
+    id: 4,
+    username: "Jon Snow",
+    avatar: `${ image }`,
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "500DT",
+  },
+  {
+    id: 5,
+    username: "Jon Snow",
+    avatar: `${ image }`,
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "500DT",
+  },
+  {
+    id: 6,
+    username: "Jon Snow",
+    avatar: `${ image }`,
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "500DT",
+  },
+  {
+    id: 7,
+    username: "Jon Snow",
+    avatar: `${ image }`,
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "500DT",
+  },
+];
+
+const UserList = () => {
+  return (
+    <div className="userList">
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+        />
+      </div>
+    </div>
+  );
+};
+
 export default UserList;
