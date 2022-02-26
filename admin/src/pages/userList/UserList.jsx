3cc2ed3492310f3c74e91cc273cd03837aa832c8
malekaridhi <malek.aridhii@gmail.com>
2022-02-26 14:50:46 +0100

    userlist route

--- admin/src/pages/userList/UserList.jsx
+++ admin/src/pages/userList/UserList.jsx
@@ -1,37 +1,33 @@
 import React from 'react';
 import "./userList.css"
 import { DataGrid } from '@mui/x-data-grid';
+import image from "../../images/a2.jpg"
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: 'username', headerName: 'Username', width: 130 },
+    { field: 'email', headerName: 'Email', width: 130 },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
+      field: 'status',
+      headerName: 'Status',
       width: 90,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
+        field: 'transaction',
+        headerName: 'Transaction',
+        width: 90,
+      },
+    ,
   ];
   
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
+    { id: 2, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
+    { id: 3, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
+    { id: 4, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
+    { id: 5, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
+    { id: 6, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
+    { id: 7, username: 'Jon Snow', avatar: {image}, email: "jon@gmail.com",status:"active",transaction:"500DT" },
+
   ];
   
 const UserList = () => {
