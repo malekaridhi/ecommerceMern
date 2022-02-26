568af9cb036d991e88f2d924cb87f97052a99cdb
malekaridhi <malek.aridhii@gmail.com>
2022-02-26 14:21:42 +0100

    userlist route

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,21 +1,30 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
-import "./app.css"
+import "./app.css";
 import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 function App() {
   return (
-    <div>
-    <Topbar/>
-    <div className="container">
-      <Sidebar/>
-      
-      {/* <div className="others">
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+
+        {/* <div className="others">
       otherpages
-      </div> */}
-      <Home/>
-      
-    </div>
-    </div>
+    </div> */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users" element={<UserList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
