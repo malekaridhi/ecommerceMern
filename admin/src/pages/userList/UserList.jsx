c12dc8c8a7740f441337dc93ab2b53a611b25548
malekaridhi <malek.aridhii@gmail.com>
2022-02-26 14:28:04 +0100

    userlist route

--- admin/src/pages/userList/UserList.jsx
+++ admin/src/pages/userList/UserList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./userList.css"
+import { DataGrid } from '@mui/x-data-grid';
 const UserList = () => {
     return ( 
         <div className="userList">
-
+hi there
         </div>
      );
 }
