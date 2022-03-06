17d39e7a08f8178e282be8d6d2b60c5e2c97a21f
malekaridhi <malek.aridhii@gmail.com>
2022-03-06 06:40:57 +0100

    user edit page

--- admin/src/pages/user/User.jsx
+++ admin/src/pages/user/User.jsx
@@ -1,59 +1,100 @@
-import React from 'react';
-import "./user.css"
-import image from "../../images/b3.jpg"
-import Avatar from '@mui/material/Avatar';
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import React from "react";
+import "./user.css";
+import image from "../../images/b3.jpg";
+import Avatar from "@mui/material/Avatar";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LocationSearchingIcon from "@mui/icons-material/LocationSearching";
 const User = () => {
-    return ( 
-        <div className='user'>
-            <div className="userTitleContainer">
-                <h1 className='userTitle'>Edit User</h1>
-                <button className="userAddButton">Create</button>    
+  return (
+    <div className="user">
+      <div className="userTitleContainer">
+        <h1 className="userTitle">Edit User</h1>
+        <button className="userAddButton">Create</button>
+      </div>
+      <div className="userContainer">
+        <div className="userShow">
+          <div className="userShowTop">
+            <Avatar src={image} className="userShowImg" />
+            <div className="userShowTopTitle">
+              <span className="userShowUsername">Rachel Green</span>
+              <span className="userShowUserTitle">Web Developper</span>
             </div>
-            <div className="userContainer">
-                <div className="userShow">
-                    <div className="userShowTop">
-                        <Avatar src={image}  className="userShowImg" />
-                        <div className="userShowTopTitle">
-                            <span className="userShowUsername">Rachel Green</span>
-                            <span className="userShowUserTitle">Web Developper</span>
-
-                        </div>
-                    </div>
-                    <div className="userShowBottom">
-                        <span className="userShowTitle">Account Details</span>
-                        <div className="userShowInfo">
-                           <PermIdentityIcon className='userShowIcon'/>
-                           <span className="userShowInfoTitle">rachel888</span>
-                        </div>
-                        <div className="userShowInfo">
-                           <CalendarTodayIcon className='userShowIcon'/>
-                           <span className="userShowInfoTitle">12.02.1978</span>
-                        </div>
-                        <span className="userShowTitle">Contact Details</span>
-                        <div className="userShowInfo">
-                           <PhoneAndroidIcon className='userShowIcon'/>
-                           <span className="userShowInfoTitle">+216 20 555 555</span>
-                        </div>
-
-                        <div className="userShowInfo">
-                           <MailOutlineIcon className='userShowIcon'/>
-                           <span className="userShowInfoTitle">rachel888@gmail.com</span>
-                        </div>
-                        <div className="userShowInfo">
-                           <LocationSearchingIcon className='userShowIcon'/>
-                           <span className="userShowInfoTitle">Tunis | Tunisia</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="userUpdate"></div>
+          </div>
+          <div className="userShowBottom">
+            <span className="userShowTitle">Account Details</span>
+            <div className="userShowInfo">
+              <PermIdentityIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">rachel888</span>
             </div>
+            <div className="userShowInfo">
+              <CalendarTodayIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">12.02.1978</span>
+            </div>
+            <span className="userShowTitle">Contact Details</span>
+            <div className="userShowInfo">
+              <PhoneAndroidIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">+216 20 555 555</span>
+            </div>
+
+            <div className="userShowInfo">
+              <MailOutlineIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">rachel888@gmail.com</span>
+            </div>
+            <div className="userShowInfo">
+              <LocationSearchingIcon className="userShowIcon" />
+              <span className="userShowInfoTitle">Tunis | Tunisia</span>
+            </div>
+          </div>
         </div>
-     );
-}
- 
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form className="userUpdateForm">
+            <div className="userUpdateLeft">
+              <div className="userUpdateItem">
+                <label>Username</label>
+                <input type="text"
+                 className="userUpdateInput" 
+                 placeholder="rachelgreen888"
+                 />
+              </div>
+              <div className="userUpdateItem">
+                <label>Full Name</label>
+                <input type="text"
+                 className="userUpdateInput" 
+                 placeholder="Rachel Green"
+                 />
+              </div>
+              <div className="userUpdateItem">
+                <label>Email</label>
+                <input type="text"
+                 className="userUpdateInput" 
+                 placeholder="rachelgreen888@gmail.com"
+                 />
+              </div>
+              <div className="userUpdateItem">
+                <label>Phone</label>
+                <input type="text"
+                 className="userUpdateInput" 
+                 placeholder="+216 20 555 555"
+                 />
+              </div>
+              <div className="userUpdateItem">
+                <label>Adress</label>
+                <input type="text"
+                 className="userUpdateInput" 
+                 placeholder="Tunis | Tunisia"
+                 />
+              </div>
+            </div>
+            <div className="userUpdateRight"></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default User;
