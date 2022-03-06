579e17afb11903de94a1c3e74f43c9cbc5159c3b
malekaridhi <malek.aridhii@gmail.com>
2022-03-06 07:34:30 +0100

    user edit page

---
+++ admin/src/pages/newUser/NewUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./newUser.css"
+
+const NewUser = () => {
+    return ( 
+        <div className="newUser">
+            
+        </div>
+     );
+}
+ 
+export default NewUser;
