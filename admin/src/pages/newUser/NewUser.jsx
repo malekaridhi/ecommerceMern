7c6337a0a3caf29c9a0c7d1b591db3896d7a4cb2
malekaridhi <malek.aridhii@gmail.com>
2022-03-07 06:14:58 +0100

    user edit page

--- admin/src/pages/newUser/NewUser.jsx
+++ admin/src/pages/newUser/NewUser.jsx
@@ -11,29 +11,44 @@ const NewUser = () => {
                    <input type="text" placeholder='harry'/>
                </div>
                <div className="newUserItem">
-                   <label>Full name</label>
-                   <input type="text" placeholder='harry'/>
+                   <label>Full Name</label>
+                   <input type="text" placeholder='Harry Potter'/>
                </div>
                <div className="newUserItem">
-                   <label>Username</label>
-                   <input type="text" placeholder='harry'/>
+                   <label>Email</label>
+                   <input type="text" placeholder='harry@gmail.com'/>
                </div>
                <div className="newUserItem">
-                   <label>Username</label>
-                   <input type="text" placeholder='harry'/>
+                   <label>Password</label>
+                   <input type="password" placeholder='password'/>
                </div>
                <div className="newUserItem">
-                   <label>Username</label>
-                   <input type="text" placeholder='harry'/>
+                   <label>Phone</label>
+                   <input type="text" placeholder='+216 20 666 666'/>
                </div>
                <div className="newUserItem">
-                   <label>Username</label>
-                   <input type="text" placeholder='harry'/>
+                   <label>Address</label>
+                   <input type="text" placeholder='Hogworts school | UK'/>
                </div>
                <div className="newUserItem">
-                   <label>Username</label>
-                   <input type="text" placeholder='harry'/>
+                   <label>Gender</label>
+                   <div className="newUserGender">
+                       <input type="radio" name='gender' id='male' value="male" />
+                       <label htmlFor="male">Male</label>
+                       <input type="radio" name='gender' id='Female' value="Female" />
+                       <label htmlFor="female">Female</label>
+                       <input type="radio" name='gender' id='other' value="other" />
+                       <label htmlFor="other">Other</label>
+                   </div>
                </div>
+               <div className="newUserItem">
+                   <label>Active</label>
+                   <select name="active" id="active" className="newUserSelect">
+                       <option value="yes">Yes</option>
+                       <option value="no">No</option>
+                   </select>
+               </div>
+               <button className="newUserButton">Create</button>
            </form>
         </div>
      );
