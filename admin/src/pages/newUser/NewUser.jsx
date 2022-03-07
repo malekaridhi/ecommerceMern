2bd2e845b75cd49696143c8265d8772dd8156ca4
malekaridhi <malek.aridhii@gmail.com>
2022-03-07 06:03:16 +0100

    user edit page

--- admin/src/pages/newUser/NewUser.jsx
+++ admin/src/pages/newUser/NewUser.jsx
@@ -4,7 +4,37 @@ import "./newUser.css"
 const NewUser = () => {
     return ( 
         <div className="newUser">
-           new User 
+           <h1 className="userTitle">New User</h1> 
+           <form  className="newUserForm">
+               <div className="newUserItem">
+                   <label>Username</label>
+                   <input type="text" placeholder='harry'/>
+               </div>
+               <div className="newUserItem">
+                   <label>Full name</label>
+                   <input type="text" placeholder='harry'/>
+               </div>
+               <div className="newUserItem">
+                   <label>Username</label>
+                   <input type="text" placeholder='harry'/>
+               </div>
+               <div className="newUserItem">
+                   <label>Username</label>
+                   <input type="text" placeholder='harry'/>
+               </div>
+               <div className="newUserItem">
+                   <label>Username</label>
+                   <input type="text" placeholder='harry'/>
+               </div>
+               <div className="newUserItem">
+                   <label>Username</label>
+                   <input type="text" placeholder='harry'/>
+               </div>
+               <div className="newUserItem">
+                   <label>Username</label>
+                   <input type="text" placeholder='harry'/>
+               </div>
+           </form>
         </div>
      );
 }
