a67d9378de587b123dec784b70343b6373a33be9
malekaridhi <malek.aridhii@gmail.com>
2022-03-07 08:06:30 +0100

    product list

--- admin/src/pages/product/Product.jsx
+++ admin/src/pages/product/Product.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import "./product.css"
-
+import { Link } from "react-router-dom";
 const Product = () => {
     return ( 
         <div className="product">
             <div className="productTitleContainer">
                 <h1 className="productTitle">Product</h1>
+                <Link to="newProduct">
                 <button className="productAddButton">Create</button>
+                </Link>
             </div>
-            
+            <div className="productTop">
+                <div className="productTopLeft">
+
+                </div>
+                <div className="productTopRight">
+                    <img src="" alt="" className="productInfoImage" />
+                    <span className="productName">Name</span>
+                </div>
+            </div>
+            <div className="productBottom">
+
+            </div>
         </div>
      );
 }
