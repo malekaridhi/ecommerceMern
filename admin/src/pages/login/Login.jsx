8d7af0b3c2b24daa024edf5a18842a412148b125
malekaridhi <malek.aridhii@gmail.com>
2022-03-09 14:51:18 +0100

    redux

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import "./login.css";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/reducers/apiReq";
+import {useSelector} from "react-redux"
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
+  const state = useSelector(state=>state)
+  console.log(state)
   const changeUsername = (e) => {
     setUsername(e.target.value);
   };
