0ca7950ec30661402aaa3c62a32b15678cc8b2bb
malekaridhi <malek.aridhii@gmail.com>
2022-03-09 10:43:12 +0100

    login

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -4,14 +4,22 @@ import React,{useState} from 'react';
 const Login = () => {
     const [user,setUser]=useState("")
     const [password,setPassword]=useState("")
-    
+     
+    const changeUsername = (e)  => {
+        setUser(e.target.value);
+    }
+    const changePassword = (e)  => {
+        setPassword(e.target.value);
+    }
     return ( 
     <div >
         <input type="text" 
         placeholder='username'
+        onChange={changeUsername}
         />
          <input type="password" 
         placeholder='password'
+        onChange={changePassword}
         />
         <button>Login</button>
 
