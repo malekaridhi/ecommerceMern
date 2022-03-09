1e5ae4009e05160a293ef63f4b8c6805517c8848
malekaridhi <malek.aridhii@gmail.com>
2022-03-09 11:37:56 +0100

    redux

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
+import { PersistGate } from "redux-persist/integration/react";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+// const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+// let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ admin/src/redux/userRedux.js
@@ -0,0 +1,25 @@
+import {createSlice} from "@reduxjs/toolkit"
+const userSlice = createSlice({
+    name: "user",
+    initialState: {
+      currentUser: null,
+      isFetching:false,
+      error: false,
+    },
+    reducers: {
+      startLogin: (state ) => {
+        state.isFetching = true;
+      },
+      successLogin: (state ,action) => {
+        state.isFetching = false
+        state.currentUser = action.payload;
+      },
+      faledLogin: (state) => {
+        state.isFetching = false
+        state.error = true;
+      },
+    },
+  });
+  
+  export const { startLogin, successLogin, faledLogin } = userSlice.actions;
+  export default userSlice.reducer;
