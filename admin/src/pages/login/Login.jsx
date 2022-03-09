a276e893b778d8c7e97509e54e0938b0c4337aac
malekaridhi <malek.aridhii@gmail.com>
2022-03-09 10:39:45 +0100

    login

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 
 const Login = () => {
+    const [user,setUser]=useState("")
+    const [password,setPassword]=useState("")
+    
     return ( 
     <div >
         <input type="text" 
         placeholder='username'
         />
+         <input type="password" 
+        placeholder='password'
+        />
+        <button>Login</button>
 
         </div> );
 }
