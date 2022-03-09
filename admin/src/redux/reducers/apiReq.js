c32dee992c5cb5104f640aef2f18b33b1cf9923f
malekaridhi <malek.aridhii@gmail.com>
2022-03-09 11:40:59 +0100

    redux

---
+++ admin/src/redux/reducers/apiReq.js
@@ -0,0 +1,16 @@
+import { startLogin, successLogin, faledLogin } from "../userRedux";
+
+import { publicRequest } from "../../reqMethod";
+
+export const login = async(dispatch, user) =>{
+ dispatch(startLogin());
+ try {
+     const res = await publicRequest.post("/auth/login",user);
+     console.log(res.data)
+     dispatch(successLogin(res.data))
+ } catch (error) {
+     dispatch(faledLogin())
+ }
+
+
+}
