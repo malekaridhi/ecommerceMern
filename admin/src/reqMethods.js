a6a0ba39a1d45f4e0c379388b769e365465e79a5
malekaridhi <malek.aridhii@gmail.com>
2022-03-12 21:31:03 +0100

    redux

--- admin/src/reqMethods.js
+++ admin/src/reqMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
  const BASE_URL = "http://localhost:8000/api/"
  const TOKEN = process.env.REACT_APP_TOKEN
+ console.log(localStorage.getItem("token"))
  export const publicRequest =  axios.create({
      baseURL:BASE_URL,
  });
