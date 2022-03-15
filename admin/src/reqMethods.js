808f253ca409edee8715d0bd3e64abfb4a315050
malekaridhi <malek.aridhii@gmail.com>
2022-03-15 22:40:05 +0100

    redux

--- admin/src/reqMethods.js
+++ admin/src/reqMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  const BASE_URL = "http://localhost:8000/api/"
  const TOKEN = process.env.REACT_APP_TOKEN
- console.log(JSON.parse(localStorage.getItem("persist:root")).currentUser)
+ console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser))
  export const publicRequest =  axios.create({
      baseURL:BASE_URL,
  });
