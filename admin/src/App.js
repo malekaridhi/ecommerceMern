54f9a56a5993536f9566950d0b32ac32f685d55b
malekaridhi <malek.aridhii@gmail.com>
2022-03-18 22:38:38 +0100

    redux

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,11 +2,7 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -15,28 +11,32 @@ import Product from "./pages/product/Product";
 import NewPrpduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 function App() {
+  const admin = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin;
   return (
     <Router>
       <Routes>
-      <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
-    {/* </Router>
+      {/* </Router>
     <Router> */}
-    { <> <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/users" element={<UserList />} />
-          <Route exact path="/users/:userId" element={<User/>} />
-          <Route exact path="/newUser" element={<NewUser/>} />
-          <Route exact path="/products" element={<ProductList/>} />
-          <Route exact path="/product/:productId" element={<Product/>} />
-          <Route exact path="/newProduct" element={<NewPrpduct/>} />
-          <Route exact path="/login" element={<Login/>} />
-        </Routes>
-      </div>
-      </>}
+      {
+        <>
+          <Topbar />
+          <div className="container">
+            <Sidebar />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/users" element={<UserList />} />
+              <Route exact path="/users/:userId" element={<User />} />
+              <Route exact path="/newUser" element={<NewUser />} />
+              <Route exact path="/products" element={<ProductList />} />
+              <Route exact path="/product/:productId" element={<Product />} />
+              <Route exact path="/newProduct" element={<NewPrpduct />} />
+              <Route exact path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </>
+      }
     </Router>
   );
 }
