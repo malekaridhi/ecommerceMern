b13360bb4cc8982b9ec608c38317eeeac54ce8c5
malekaridhi <malek.aridhii@gmail.com>
2022-03-19 22:31:58 +0100

    redux

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -9,6 +9,7 @@ const WidgetSm = () => {
     useEffect(() => {
       const getusers = async () =>{
           const res = await userRequest.get("user/?new=true")
+          setUsers(res.data)
       }
     }, [])
     
