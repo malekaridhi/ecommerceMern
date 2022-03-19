c391895099ed3ddcdd754046889fee7adc5f8951
malekaridhi <malek.aridhii@gmail.com>
2022-03-19 21:33:22 +0100

    redux

--- admin/src/components/widgetSm/WidgetSm.jsx
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./widgetSm.css"
 import image from "../../images/a4.jpg"
 import VisibilityIcon from '@mui/icons-material/Visibility';
 const WidgetSm = () => {
+    const [users,setUsers] = useState([])
     return ( 
         <div className="widgetsm">
          <span className="widgetsmTitle">New Join Members</span>
