8fefcbf80e00ce9972d957002a65e0e391e26655
malekaridhi <malek.aridhii@gmail.com>
2022-04-10 17:13:55 +0100

    redux

--- admin/src/redux/productRedux.js
+++ admin/src/redux/productRedux.js
@@ -9,59 +9,59 @@ export const productSlice = createSlice({
   },
   reducers: {
     //GET ALL
-    getProductStart: (state) => {
+    startGetttingProduct: (state) => {
       state.isFetching = true;
       state.error = false;
     },
-    getProductSuccess: (state, action) => {
+    successGettingProduct: (state, action) => {
       state.isFetching = false;
       state.products = action.payload;
     },
-    getProductFailure: (state) => {
+   failingGettingProduct: (state) => {
       state.isFetching = false;
       state.error = true;
     },
     //DELETE
-    deleteProductStart: (state) => {
+    startDeletingProduct: (state) => {
       state.isFetching = true;
       state.error = false;
     },
-    deleteProductSuccess: (state, action) => {
+    successDeletingProduct: (state, action) => {
       state.isFetching = false;
       state.products.splice(
         state.products.findIndex((item) => item._id === action.payload),
         1
       );
     },
-    deleteProductFailure: (state) => {
+    failingdeletingProduct: (state) => {
       state.isFetching = false;
       state.error = true;
     },
     //UPDATE
-    updateProductStart: (state) => {
+    startUpdatingProduct: (state) => {
       state.isFetching = true;
       state.error = false;
     },
-    updateProductSuccess: (state, action) => {
+    successupdatingProduct: (state, action) => {
       state.isFetching = false;
       state.products[
         state.products.findIndex((item) => item._id === action.payload.id)
       ] = action.payload.product;
     },
-    updateProductFailure: (state) => {
+    failingupdatingProduct: (state) => {
       state.isFetching = false;
       state.error = true;
     },
-    //UPDATE
-    addProductStart: (state) => {
+    //ADD
+    startAddingProduct: (state) => {
       state.isFetching = true;
       state.error = false;
     },
-    addProductSuccess: (state, action) => {
+    successAddingProduct: (state, action) => {
       state.isFetching = false;
       state.products.push(action.payload);
     },
-    addProductFailure: (state) => {
+    failureAddingProduct: (state) => {
       state.isFetching = false;
       state.error = true;
     },
@@ -69,18 +69,9 @@ export const productSlice = createSlice({
 });
 
 export const {
-  getProductStart,
-  getProductSuccess,
-  getProductFailure,
-  deleteProductStart,
-  deleteProductSuccess,
-  deleteProductFailure,
-  updateProductStart,
-  updateProductSuccess,
-  updateProductFailure,
-  addProductStart,
-  addProductSuccess,
-  addProductFailure,
+    startGetttingProduct,
+    successGettingProduct,
+    failingGettingProduct,
 } = productSlice.actions;
 
 export default productSlice.reducer;
